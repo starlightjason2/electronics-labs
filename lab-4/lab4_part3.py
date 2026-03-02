be3dcc1d6b8e604bc3f744e5116736308276fc69
Jason Jain <starlightjason2@gmail.com>
2026-03-02 00:49:37 -0800

    lab 4 progr

--- lab-4/lab4_part3.py
+++ lab-4/lab4_part3.py
@@ -5,21 +5,23 @@ import pandas as pd
 
 from utils.paths import get_paths
 from utils.utils import (
-    create_bode_plot,
-    create_phase_plot,
-    plot_oscilloscope_data,
+    find_closest_index,
     load_oscilloscope_data,
 )
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 
 paths = get_paths(__file__)
 
+f0 = 400
+omega_c = 1 / (20e-6)
 
 low_pass_df = load_oscilloscope_data("low_pass_400Hz", paths.data_dir)
 fft_df = pd.read_csv(paths.data_dir / "fft_low_pass_400Hz.txt.csv")
+fft_df["n"] = fft_df["f_in"] / f0
 
-omega_c = 1 / (20e-6)
-
+ax1: Axes
+ax2: Axes
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 
 ax1.plot(
@@ -30,10 +32,24 @@ ax1.set_ylabel(r"Voltage $V_{\mathrm{Out}}$ (V)")
 ax1.legend(loc="lower right")
 ax1.grid(True, alpha=0.3)
 
-ax2.scatter(fft_df["f_in"], fft_df["amplitude"])
-ax2.set_xlabel("Frequency (Hz)")
+ax2.scatter(fft_df["n"], fft_df["amplitude"], zorder=100, label="FFT Experimental")
+ax2.set_xlabel("$n$th Harmonic")
 ax2.set_ylabel("Fourier Coefficient")
 ax2.grid(True, alpha=0.3)
+
+amp = np.max(low_pass_df["v_in"])
+n = np.arange(1, 10, 1)
+fft_coeffs = (1 / (n * np.pi)) * (2 - 2 * (-1) ** n)
+scale = fft_df["amplitude"][find_closest_index(fft_df["f_in"], f0)] / fft_coeffs[0]
+ax2.stem(
+    n,
+    fft_coeffs * scale,
+    linefmt="tomato",
+    markerfmt="ro",
+    basefmt=" ",
+    label="FFT Theoretical",
+)
+ax2.legend()
 
 plt.tight_layout()
 plt.savefig(paths.output_dir / f"fft_low_pass_400Hz.png", dpi=150)
--- lab-4/lab4_part4.py
+++ lab-4/lab4_part4.py
@@ -4,12 +4,7 @@ import numpy as np
 import pandas as pd
 
 from utils.paths import get_paths
-from utils.utils import (
-    create_bode_plot,
-    create_phase_plot,
-    plot_oscilloscope_data,
-    load_oscilloscope_data,
-)
+from utils.utils import find_closest_index, load_oscilloscope_data
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 
@@ -21,7 +16,15 @@ low_pass_df = pd.read_csv(
 fft_df = pd.read_csv(paths.data_dir / "fft_low_pass_pulse_200Hz.csv")
 low_pass_df["t_in"] = low_pass_df["t_in"] - low_pass_df["t_in"].min()
 
-omega_c = 1 / (20e-6)
+
+f0 = 200
+threshold = np.max(low_pass_df["v_in"]) / 2
+pulse_samples = (low_pass_df["v_in"] > threshold).sum()
+dt = low_pass_df["t_in"].diff().median()
+pulse_width = pulse_samples * dt
+T = low_pass_df["t_in"].max()
+
+fft_df["n"] = fft_df["f_in"] / f0
 
 ax1: Axes
 ax2: Axes
@@ -31,18 +34,30 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 ax1.plot(
     low_pass_df["t_in"], low_pass_df["v_in"], label="Voltage In", color="goldenrod"
 )
-ax1.set_xlabel(r"Time $t$ (μs)")
+ax1.set_xlabel(r"Time $t$ (s)")
 ax1.set_ylabel(r"Voltage $V_{\mathrm{Out}}$ (V)")
 
 ax1.legend(loc="lower right")
 ax1.grid(True, alpha=0.3)
 
-ax2.set_xlabel("Frequency (Hz)")
+ax2.set_xlabel("$n$th Harmonic")
 ax2.set_ylabel("Fourier Coefficient")
 ax2.grid(True, alpha=0.3)
 
 # experimental
-ax2.scatter(fft_df["f_in"], fft_df["amplitude"], label="Experimental FT")
+ax2.scatter(fft_df["n"], fft_df["amplitude"], zorder=100, label="FFT Experimental")
+# theory
+threshold = np.max(low_pass_df["v_in"]) / 2
+duty_cycle = pulse_width / T
+fft_coeffs = np.abs(duty_cycle * np.sinc(fft_df["n"] * duty_cycle))
+scale = fft_df["amplitude"].max() / fft_coeffs.max()
+print(duty_cycle)
+ax2.plot(
+    fft_df["n"],
+    fft_coeffs * scale,
+    color="tomato",
+)
+
 
 fig.legend()
 fig.tight_layout()
